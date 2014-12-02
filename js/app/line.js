5386cc15cee6a95e3f16b2b6284f38980b4a177d
Nico Zhi <tczdf123@hotmail.com>
2014-12-02 18:32:25 +0800

    Bascic Construction
    6 files added

---
+++ js/app/line.js
@@ -0,0 +1,10 @@
+define(['jquery'], function ($) {
+    "use strict";
+    var Line = function () {};
+
+    Line.prototype = {
+        constructor: Line,
+    };
+
+    return Line;
+});
---
+++ js/app/node.js
@@ -0,0 +1,10 @@
+define(['jquery', 'line'], function ($, Line) {
+    "use strict";
+    var Node = function () {};
+
+    Node.prototype = {
+        constructor: Node,
+    };
+
+    return Node;
+});
